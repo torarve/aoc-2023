5d9c1be83a3430d1e3f6a99618cec0d9ade49085
Tor Arve Stangeland <tor.stangeland@bouvet.no>
2023-12-05 10:26:00 +0100

    Solved!

--- day05.py
+++ day05.py
@@ -40,9 +40,43 @@ humidity-to-location map:
 60 56 37
 56 93 4""".split("\n")
 
-lines = read_file()
 
-# print(lines)
+"""seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2: 98-99 => 50-51
+52 50 48: 
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+
+
+lines = read_file()
 
 seeds = [int(x) for x in re.findall(r"\d+", lines[0])]
 
@@ -67,7 +101,6 @@ location = seeds[0]
 for location in seeds:
     current = "seed"
     while True:
-        # print(f"{current} {location} -> ")
         next = [x for x in maps.keys() if x.startswith(current)]
         if len(next) == 0:
             break
@@ -81,7 +114,6 @@ for location in seeds:
             location = destStart + (location-srcStart)
         
         current = key[len(current)+len("-to-"):]
-    # print(location)
     locations.append(location)
     
 print(min(locations))
@@ -91,14 +123,16 @@ def split_range(range: tuple[int,int], dStart, sStart, dLength):
     """Split range according to mapping.
     Returns mapped range and any excess"""
     start, length = range
-    if start>=sStart+dLength or start+length<=sStart:
+
+    end = start + length - 1
+    sEnd = sStart + dLength - 1
+    if (start>sStart+dLength-1 or start+length-1<sStart) or (sEnd < start or end < sStart):
         return None, [range]
     
-    # convering
+    # covering
     if sStart<=start and start+length<=sStart+dLength:
-        if dStart+(start-sStart) ==0:
-            pass
         return (dStart + (start-sStart), length), []
+
     # beginning
     if sStart<=start and start+length>sStart+dLength:
         offset = start-sStart
@@ -106,22 +140,24 @@ def split_range(range: tuple[int,int], dStart, sStart, dLength):
         # -----
         #    -------
         # ooo
-        if dStart+offset ==0:
-            pass
         return (dStart + offset, dLength-offset), [(start + (dLength - offset), length - (dLength-offset))]
+
     # end
     if sStart>start and start+length<=sStart+dLength:
+        # 0123456789
+        #      -----
+        # -------
         offset = sStart-start
-        if dStart ==0:
-            pass
         return (dStart, length-offset), [(start, offset)]
+
     # inside
     if sStart>start and start+length>sStart+dLength:
+        # 0123456789
+        #    ----
+        # ----------
         offset = sStart - start
-        if dStart+offset ==0:
-            pass
-        return (dStart+offset, dLength), [(start, offset), (start+offset+dLength, length-dLength-offset)]
-    
+        return (dStart, dLength), [(start, offset), (start+offset+dLength, length-dLength-offset)]
+
     print(range, dStart, sStart, dLength)
     raise "Should not happend"
 
@@ -129,7 +165,6 @@ def split_range(range: tuple[int,int], dStart, sStart, dLength):
 locations = []
 location = seeds[0]
 for i in range(len(seeds)//2):
-    # print(i, seeds[2*i:2*(i+1)])
     pos, length = tuple(seeds[2*i:2*(i+1)])
     working_set: list[tuple[int,int]] = [(pos,length)]
     current = "seed"
@@ -144,25 +179,20 @@ for i in range(len(seeds)//2):
         # For each range
         for start, length in working_set:
             remaining = [(start, length)]
-            # Find each overlapping range
-            dest = [(destStart, srcStart, length)
-                    for (destStart, srcStart, length) in (ranges)
-                    if not (start+length<=srcStart or start>srcStart+length)]
-            for dStart, sStart, dLength in dest:
+            for dStart, sStart, dLength in ranges:
                 rr = []
                 for r in remaining:
                     a, b = split_range(r, dStart, sStart, dLength)
                     if a is not None:
                         temp.append(a)
                     rr.extend(b)
-                remaining = rr        
+                remaining = rr
+
             temp.extend(remaining)
             
         working_set = temp
-        # print(temp)
         current = key[len(current)+len("-to-"):]
     locations.append(min([a for a,b in working_set]))
-    print(f"{i+1} of {len(seeds)//2}")
-    print(min(locations))
     
+
 print(min(locations))
